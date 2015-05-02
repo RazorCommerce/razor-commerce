659c115513a29a59ae0340552345b09995dec625
Joel Milne <joel@goldhat.ca>
2015-05-01 19:46:24 -0500

    adding pagination to customer list

--- razor/single_pages/dashboard/razor/customers.php
+++ razor/single_pages/dashboard/razor/customers.php
@@ -20,4 +20,13 @@
       <?php endforeach; endif; ?>
     </tbody>
   </table>
+  
+  <div class="col-md-12 text-center">
+    <?php
+      if( $paginator->getTotalPages() > 1 ) {
+        print $pagination;
+      }
+    ?>
+  </div>
+  
 </div>
