65999acc6b763c7ee12cbfbb9993a79b5f2f4c6e
Joel Milne <joel@goldhat.ca>
2015-05-04 00:58:43 -0700

    ecommerce icon blocks output changed to enable fonts to be changed with font-awesome override

--- razor/blocks/commerce_icon/view.php
+++ razor/blocks/commerce_icon/view.php
@@ -5,9 +5,8 @@ defined('C5_EXECUTE') or die("Access Denied.");
 <div class="commerce-icon">
   <h3>
     <a href="<?php print $url; ?>">
-      <i class="fa <?php print $icon; ?>">
-        <span><?php print $text; ?></span>
-      </i>
+      <i class="fa <?php print $icon; ?>"></i>
+      <span><?php print $text; ?></span>
     </a>
   </h3>
 </div>
