48228ddc3a160b9d1e610bd8803b12989c34cc44
Joel Milne <joel@goldhat.ca>
2015-05-05 19:34:10 -0700

    adding install Att. Key Category commerce_product

--- razor/src/Install/Install.php
+++ razor/src/Install/Install.php
@@ -6,6 +6,8 @@ use Razor\Core\Install\Fields as InstallFields;
 use Razor\Core\Install\Composer as InstallComposer;
 use Razor\Core\Install\Image as InstallImage;
 use Razor\Core\Install\Settings as InstallSettings;
+use Concrete\Core\Attribute\Key\Category as AttributeKeyCategory;
+use Concrete\Core\Attribute\Type as AttributeType;
 
 class Install {
 
@@ -18,9 +20,27 @@ class Install {
 
     $installComposer = new InstallComposer();
     $installComposer->setupComposer();
-    
+
     $installSettings = new InstallSettings;
     $installSettings->defaults();
+
+    $this->installAttributeKeys( $pkg );
+
+  }
+
+  public function installAttributeKeys( $pkg ) {
+
+    //create custom attribute category for products
+    $cpAKC = AttributeKeyCategory::getByHandle('commerce_product');
+    if (!is_object($cpAKC)) {
+      $cpAKC = AttributeKeyCategory::add( 'commerce_product', AttributeKeyCategory::ASET_ALLOW_SINGLE, $pkg );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('text') );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('textarea') );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('number') );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('address') );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('boolean') );
+      $cpAKC->associateAttributeKeyType( AttributeType::getByHandle('date_time') );
+    }
 
   }
 
