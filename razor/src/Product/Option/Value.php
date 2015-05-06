593358659828ca735588cd4551bcf31277355a38
Joel Milne <joel@goldhat.ca>
2015-05-06 15:35:42 -0700

    sketching out object files needed for new approach to product options

---
+++ razor/src/Product/Option/Value.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Razor\Core\Product\Option;
+
+use \Concrete\Core\Foundation\Object;
+use Database;
+use Razor\Core\Product\Product;
+use \Concrete\Package\Razor\Src\Attribute\Key\ProductKey;
+use \Concrete\Package\Razor\Src\Attribute\Value\ProductValue;
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class Option extends Object {
+
+
+}
---
+++ razor/src/Product/ProductOption.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Razor\Core\Product;
+
+use \Concrete\Core\Foundation\Object;
+use Database;
+use Razor\Core\Product\Product;
+use \Concrete\Package\Razor\Src\Attribute\Key\ProductKey;
+use \Concrete\Package\Razor\Src\Attribute\Value\ProductValue;
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class ProductOption extends Object {
+
+
+
+}
