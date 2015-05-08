8a2ec298c302a26edc87fc8c6eee5d228d7604f5
Joel Milne <joel@goldhat.ca>
2015-05-08 10:44:06 -0700

    fixes #94 install error

--- razor/src/Product/ProductList.php
+++ razor/src/Product/ProductList.php
@@ -3,15 +3,14 @@
 namespace Razor\Core\Product;
 
 use Database;
-use Page;
+use \Concrete\Core\Search\ItemList\Database\AttributedItemList;
 
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
 class ProductList extends AttributedItemList {
 
-  protected function getAttributeKeyClassName()
-  {
-      return '\\Concrete\\Package\\Razor\\Core\\Attribute\\Key\\ProductKey';
+  protected function getAttributeKeyClassName() {
+    return '\\Concrete\\Package\\Razor\\Core\\Attribute\\Key\\ProductKey';
   }
 
 }
