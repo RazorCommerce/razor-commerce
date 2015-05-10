e7bbd3c0c59ff5defa8424869e9fde06a68224bf
Joel Milne <joel@goldhat.ca>
2015-05-10 01:34:15 -0700

    reorgnizing methods in product/options controller

--- razor/controllers/single_page/dashboard/razor/product/option.php
+++ razor/controllers/single_page/dashboard/razor/product/option.php
@@ -1,129 +1,93 @@
 <?php
+
 namespace Concrete\Package\Razor\Controller\SinglePage\Dashboard\Razor\Product;
 use \Concrete\Core\Page\Controller\DashboardPageController;
-
-use \Concrete\Package\Razor\Src\Attribute\Key\ProductKey;
 use Razor\Core\Product\Product;
-use Razor\Core\Product\ProductOption as ProductProductOption;
 use Razor\Core\Product\Option\Option as ProductOption;
+use Razor\Core\Product\ProductOption as ProductProductOption;
 
 class Option extends DashboardPageController {
 
-  public function on_start() {
-    // $this->set('pageTitle', 'Product Options');
-
-    $product = Product::getByID( 1 );
-
+  public function view( $productID ) {
+    $product = Product::getByID( $productID );
+    $option = new ProductOption;
+    $allOptions = $option->getAll();
     if( $product ) {
       $currentOptions = $product->option()->getAll();
       $this->set('currentOptions', $currentOptions);
+      foreach( $allOptions as $option ) {
+        $ppo = new ProductProductOption( $product );
+        $option->assigned = false;
+        if( $ppo->hasOption( $option )) {
+          $option->assigned = true;
+        }
+      }
     } else {
       $this->set('currentOptions', false);
-    }
-
-    $option = new ProductOption;
-    $allOptions = $option->getAll();
-
-    foreach( $allOptions as $option ) {
-      $ppo = new ProductProductOption( $product );
-      $option->assigned = false;
-      if( $ppo->hasOption( $option )) {
-        $option->assigned = true;
-      }
     }
     $this->set('allOptions', $allOptions);
   }
 
-  public function view() {
-
-  }
-
   public function add( $productID, $optionID = false ) {
-
-    // get data
     $editOption = ProductOption::getByID( $optionID );
     $this->set('editOption', $editOption);
     $this->set('mode', 'add');
-
   }
 
 
   public function edit( $productID, $optionID ) {
-
     $editOption = ProductOption::getByID( $optionID );
     $this->set('editOption', $editOption);
     $this->set('mode', 'edit');
-
   }
 
   public function remove( $productID, $productOptionID ) {
-
     $product = Product::getByID( $productID );
-    $productOption = ProductOption::getByID( $productOptionID ); 
+    $productOption = ProductOption::getByID( $productOptionID );
     $product->option()->remove( $productOption );
-
   }
 
   public function save() {
     $data = $this->post();
-
-    if( isset($data['save_settings'])) {
-
-      $poID = $data['poID'];
-
-      // add existing ProductOption to Product
-      if( !$poID ) {
-        $poData = array(
-          'poHandle' => uncamelcase( $data['name'] ),
-          'poName' => $data['name'],
-          'poType' => 'select',
-          'poValueDefault' => $data['default']
-        );
-        $productOption = ProductOption::add( $poData );
-
-        $values = $data['values'];
-        $values = str_replace(' ', '', $values);
-        $values = explode( ',', $values );
-        foreach( $values as $value ) {
-          $productOption->value()->add( $value );
-        }
-      } else {
-        $productOption = ProductOption::getByID( $poID );
-      }
-
-      if( $mode == 'add' ) {
-        $product = Product::getByID(1);
-        $productProductOption = $product->option()->add( $productOption );
-
-      } else {
-
-        // need way to edit the options, like addOrEdit()
-        $product = Product::getByID(1);
-        $productProductOption = $product->option()->get( $productOption );
-
-
-        $values = $data['values'];
-        $values = str_replace(' ', '', $values);
-        $values = explode( ',', $values );
-        foreach( $values as $value ) {
-          $productOption->value()->add( $value );
-        }
-
-      }
-
-
-
-      // $handle = uncamelcase( $data['name'] );
-
-      var_dump('<br /><br /><br /><br />');
-      var_dump( $data );
-      var_dump( $product );
-      var_dump( $productOption );
-      var_dump( $productProductOption );
-
+    if( !isset($data['save_settings'])) {
+      $this->redirect( '/dashboard/product/option/' );
     }
 
-    // $this->redirect( '/dashboard/product/option/edit/1/' . $poID );
+    $productID = $data['productID'];
+    $poID = $data['poID'];
+
+    // add new ProductOption
+    if( !$poID ) {
+      $poData = array(
+        'poHandle' => uncamelcase( $data['name'] ),
+        'poName' => $data['name'],
+        'poType' => 'select',
+        'poValueDefault' => $data['default']
+      );
+      $productOption = ProductOption::add( $poData );
+
+      $values = $data['values'];
+      $values = str_replace(' ', '', $values);
+      $values = explode( ',', $values );
+      foreach( $values as $value ) {
+        $productOption->value()->add( $value );
+      }
+    } else {
+      $productOption = ProductOption::getByID( $poID );
+    }
+    if( $mode == 'add' ) {
+      $product = Product::getByID( $productID );
+      $productProductOption = $product->option()->add( $productOption );
+    } else {
+      $product = Product::getByID( $productID );
+      $productProductOption = $product->option()->get( $productOption );
+      $values = $data['values'];
+      $values = str_replace(' ', '', $values);
+      $values = explode( ',', $values );
+      foreach( $values as $value ) {
+        $productOption->value()->add( $value );
+      }
+    }
   }
 
 }
